1ada81cfc675b08bebbd8458fd1ba24df5e6bcc6
Raul Domingues <99521069+Raulhdomingues@users.noreply.github.com>
2023-09-01 07:40:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function tocarSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;  //template string
+
+    tecla.onclick = function () {
+        tocarSom(idAudio);
+    }
+    
+    console.log(contador);
+
+}
